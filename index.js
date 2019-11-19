89948d4a22da45ee4674d3249f7f58508bc7de52
hydra314 <multi.goals@gmail.com>
2019-11-19 02:17:56 -0800

    barebones, but finally working

---
+++ index.js
@@ -0,0 +1,9 @@
+// const db = require('../db');
+
+module.exports = {
+   homePage: (req, res) => {
+      res.render(req, res, 'index.ejs', {
+         title: 'Jewish Women\'s Wills'
+      });
+   }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-// const db = require('../db');
+// // const db = require('../db');
 
-module.exports = {
-   homePage: (req, res) => {
-      req.app.locals.render(req, res, 'index.ejs', {
-         // title: 'Jewish Women\'s Wills'
-      });
-   }
-};
+// module.exports = {
+//    homePage: (req, res) => {
+//       res.render(req, res, 'index.ejs', {
+//          title: 'Jewish Women\'s Wills'
+//       });
+//    }
+// };
