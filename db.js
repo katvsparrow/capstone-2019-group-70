34efb730e1fcef05f96b40f13f022e21c50cce3b
hydra314 <multi.goals@gmail.com>
2020-02-13 03:38:21 -0800

    Added db creation script, callbacks for queries

---
+++ db.js
@@ -0,0 +1,166 @@
+const mysql = require('mysql');
+const fs = require('fs');
+const async = require('async');
+const MySQLStore = require('express-mysql-session');
+
+const connectionString = process.env.JAWSDB_MARIA_URL;
+
+// if env variable exists, it's on cloud; otherwise, use localhost
+const connection = connectionString
+  ? connectionString + '?multipleStatements=true'
+  : {
+      host: 'localhost',
+      user: 'root',
+      password: 'rootroot',
+      database: 'nrs',
+      multipleStatements: true
+    };
+// create a pool for more efficient connection management
+const db = mysql.createPool(connection);
+
+module.exports = {
+   // Description: retrieve language
+   // Result: [language_name]
+   getLanguage: callback => {
+      const query =
+         'SELECT name AS language_name FROM Language';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve country
+   // Result: country_name
+   getCountry: callback => {
+      const query = 
+         'SELECT name AS country_name FROM Country';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve city and corresponding country
+   // Result: city_name, country_name
+   getCity: callback => {
+      const query =
+         'SELECT City.name AS city_name, Country.name AS country_name FROM' +
+         'City INNER JOIN Country ON country_id = Country.id';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve full document
+   // Result: document title, author, year, original text, translated text, image, upload date,
+   //         language, country, city
+   getDocument: callback => {
+      const query =
+         'SELECT title, author, year, original_text, translated_text, image, upload_date,' +
+         'Language.name AS language_name, Country.name AS country_name, City.name AS city_name' +
+         'FROM Document' +
+         'INNER JOIN Language ON language_id = Language.id' +
+         'INNER JOIN Country ON country_id = Country.id' +
+         'INNER JOIN City ON city_id = City.id';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve document details for search page
+   // Result: document title, author, year, image, upload date, language, country, city
+   getDocumentMinimal: callback => {
+      const query = 
+         'SELECT title, author, year, image, upload_date,' +
+         'Language.name AS language_name, Country.name AS country_name, City.name AS city_name' +
+         'FROM Document' +
+         'INNER JOIN Language ON language_id = Language.id' +
+         'INNER JOIN Country ON country_id = Country.id' +
+         'INNER JOIN City ON city_id = City.id';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve document tag
+   // Result: tag_name
+   getDocumentTag: callback => {
+      const query =
+         'SELECT tag.name AS tag_name FROM Document' +
+         'INNER JOIN Document_Tag ON Document.id = document_id' +
+         'INNER JOIN Tag ON Tag.id = tag_id';
+      
+      db.query(query, callback);
+   },
+
+   // Description: retrieve username of document uploader
+   // Result: username
+   getDocumentPoster: callback => {
+      const query =
+         'SELECT username FROM User' +
+         'INNER JOIN Document_User ON User.id = user_id' +
+         'INNER JOIN Document ON Document.id = document_id'
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve tag
+   // Result: tag_name
+   getTag: callback => {
+      const query =
+         'SELECT name AS tag_name FROM Tag';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve role
+   // Result: role_name
+   getRole: callback => {
+      const query =
+         'SELECT name AS role_name FROM Role';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve user
+   // Result: username, email, role_name
+   getUser: callback => {
+      const query =
+         'SELECT username, email, name AS role_name FROM User' +
+         'INNER JOIN Role ON role_id = Role.id';
+
+      db.query(query, callback);
+   },
+
+   // Description: retrieve document by tag
+   // Result: document title, author, year, original text, translated text, image, upload date,
+   //         language, country, city
+   getDocumentByTag: (tag, callback) => {
+      const query =
+         'SELECT title, author, year, original_text, translated_text, image, upload_date, ' +
+         'Language.name AS language_name, Country.name AS country_name, City.name AS city_name' +
+         'FROM Document' +
+         'INNER JOIN Language ON language_id = Language.id' +
+         'INNER JOIN Country ON country_id = Country.id' +
+         'INNER JOIN City ON city_id = City.id' +
+         'INNER JOIN Document_Tag ON Document.id = document_id' +
+         'INNER JOIN Tag ON tag.id = tag_id' +
+         'WHERE Tag.name = ?';
+      const values = [tag];
+      
+      db.query(query, values, callback);
+   },
+
+   // Description: retrieve document by username
+   // Result: document title, author, year, original text, translated text, image, upload date,
+   //         language, country, city
+   getDocumentByUsername: (username, callback) => {
+      const query =
+         'SELECT title, author, year, original_text, translated_text, image, upload_date, ' +
+         'Language.name AS language_name, Country.name AS country_name, City.name AS city_name' +
+         'FROM Document' +
+         'INNER JOIN Language ON language_id = Language.id' +
+         'INNER JOIN Country ON country_id = Country.id' +
+         'INNER JOIN City ON city_id = City.id' +
+         'INNER JOIN Document_Tag ON Document.id = document_id' +
+         'INNER JOIN Tag ON tag.id = tag_id' +
+         'WHERE Tag.name = ?';
+      const values = [username];
+      
+      db.query(query, values, callback);
+   }
+}
---
+++ db/create-tables.sql
@@ -0,0 +1,127 @@
+DROP TABLE IF EXISTS Document_User;
+DROP TABLE IF EXISTS Document_Tag;
+DROP TABLE IF EXISTS Document;
+DROP TABLE IF EXISTS f573zktnzpc6ircw.Language;
+DROP TABLE IF EXISTS City;
+DROP TABLE IF EXISTS Country;
+DROP TABLE IF EXISTS Tag;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS f573zktnzpc6ircw.Role;
+
+-- Represents a single LANGUAGE
+--
+-- References: none
+-- Associations: none
+CREATE TABLE IF NOT EXISTS Language (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32) NOT NULL
+) ENGINE=InnoDB;
+
+-- Represents a single COUNTRY
+--
+-- References: none
+-- Associations: none
+CREATE TABLE IF NOT EXISTS Country (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32) NOT NULL
+) ENGINE=InnoDB;
+
+-- Represents a single CITY
+--
+-- References: Country
+-- Associations: none
+CREATE TABLE IF NOT EXISTS City (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32) NOT NULL,
+   country_id INT NOT NULL,
+   FOREIGN KEY (country_id) REFERENCES Country(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- Represents a single DOCUMENT
+--
+-- References: Language, Country, City
+-- Associations: Tag, User
+CREATE TABLE IF NOT EXISTS Document (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(128) NOT NULL,
+   author VARCHAR(64) NOT NULL,
+   year SMALLINT NOT NULL,
+   original_text MEDIUMTEXT NOT NULL,
+   translated_text MEDIUMTEXT NOT NULL,
+   image TEXT NOT NULL,
+   upload_date DATE NOT NULL,
+   language_id INT NOT NULL,
+   country_id INT,
+   city_id INT,
+   FOREIGN KEY (language_id) REFERENCES Language(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+   FOREIGN KEY (country_id) REFERENCES Country(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+   FOREIGN KEY (city_id) REFERENCES City(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- Represents a single TAG
+--
+-- References: none
+-- Associations: Document
+CREATE TABLE IF NOT EXISTS Tag (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(32) NOT NULL
+) ENGINE=InnoDB;
+
+-- Represents a single ROLE
+--
+-- References: none
+-- Associations: none
+CREATE TABLE IF NOT EXISTS Role (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(16) NOT NULL
+) ENGINE=InnoDB;
+
+-- Represents a single USER
+-- 
+-- References: Role
+-- Associations: Document
+CREATE TABLE IF NOT EXISTS User (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(32) NOT NULL UNIQUE,
+   email VARCHAR(255) NOT NULL,
+   role_id INT NOT NULL,
+   FOREIGN KEY (role_id) REFERENCES Role(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- Represents a link between a DOCUMENT and a TAG
+--
+-- References: Document, Tag
+CREATE TABLE IF NOT EXISTS Document_Tag (
+   document_id INT NOT NULL,
+   tag_id INT NOT NULL,
+   FOREIGN KEY (document_id) REFERENCES Document(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+   FOREIGN KEY (tag_id) REFERENCES Tag(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- Represents a link between a DOCUMENT and a USER
+-- 
+-- References: Document, User
+CREATE TABLE IF NOT EXISTS Document_User (
+   document_id INT NOT NULL,
+   user_id INT NOT NULL,
+   FOREIGN KEY (document_id) REFERENCES Document(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+   FOREIGN KEY (user_id) REFERENCES User(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB;
