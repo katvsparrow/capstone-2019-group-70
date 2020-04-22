55e508b7e4eed9f69d2006d90cb35f999182e0f3
Usman Nawaz <usmannawaz@Usmans-MacBook-Pro.local>
2020-04-22 03:06:28 -0700

    Enabled href tag of About Us on NavBar

--- client/src/components/Navbars/MainNavbar.jsx
+++ client/src/components/Navbars/MainNavbar.jsx
@@ -1,8 +1,8 @@
-import React from "react"; 
+import React from "react";
 import FirebaseAuthButton from "components/Login/FirebaseAuthButton";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
-// import reactstrap components 
+// import reactstrap components
 import {
     UncontrolledCollapse,
     NavbarBrand,
@@ -19,7 +19,7 @@ class MainNavbar extends React.Component {
     render() {
         return (
             <header className="header-global">
-                <Navbar 
+                <Navbar
                     className = "navbar-main navbar-transparent navbar-light bg-gradient-jww-primary"
                     expand="lg"
                     id="navbar-main"
@@ -34,7 +34,7 @@ class MainNavbar extends React.Component {
                         <button className="navbar-toggler" id="navbar_global">
                             <span className="navbar-toggler-icon" />
                         </button>
-                        
+
                         {/* Collapsed items */}
                         <UncontrolledCollapse navbar toggler="#navbar_global">
                             <div className = "navbar-collapse-header">
@@ -54,23 +54,24 @@ class MainNavbar extends React.Component {
                                     </Col>
                                 </Row>
                             </div>
-                            
+
                             {/* Left-hand Navbar content */}
                             <Nav className="navbar-nav-hover align-items-lg-center" navbar>
-                                <NavItem>   
+                                <NavItem>
                                     <NavLink href="/search">
                                         <i className="ni ni-collection d-lg-none mr-1" />
                                         <span className="nav-link-inner--text">Search</span>
                                     </NavLink>
                                 </NavItem>
-                                <NavItem>   
-                                    <NavLink href="/" onClick={e => e.preventDefault()}>
+                                <NavItem>
+
+                                    <NavLink href="/AboutUs">
                                     <i className="fa fa-info-circle d-lg-none mr-1" />
                                         <span className="nav-link-inner--text">About Us</span>
                                     </NavLink>
                                 </NavItem>
                             </Nav>
-                            
+
                             {/* Right-hand Navbar content */}
                             <Nav className="align-items-lg-center ml-lg-auto" navbar>
                                 <NavItem className="d-none d-lg-block ml-lg-4">
@@ -82,7 +83,7 @@ class MainNavbar extends React.Component {
                 </Navbar>
             </header>
         );
-    }   
+    }
 }
 
-export default MainNavbar; 
+export default MainNavbar;
