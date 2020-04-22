613bba40c3a504341f1b2b1eca81e4778fd517e3
Usman Nawaz <usmannawaz@Usmans-MacBook-Pro.local>
2020-04-22 03:13:05 -0700

    About Page

---
+++ client/src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+
+
+class AboutPage extends Component {
+
+    render() {
+        return (
+            <div>
+
+            <section className="breadcrumps-section section section-lg bg-secondary ">
+
+                <div className="container p-1 p-sm3">
+
+                    <div className="row">
+
+                        <div className="col-12">
+                            <h2>About Us</h2>
+
+                            <ol className="breadcrumb">
+                                 <li className="breadcrumb-item"><a href="/">Home</a></li>
+                                 <li className="breadcrumb-item active"> About Us </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+            <section className = "about-company-section">
+
+                    <div className="container p-1 p-sm3">
+
+                        <div className="row">
+
+                            <div className="col-12 text text-center">
+                                <h2>Welcome to Jewish Women Will's </h2>
+
+                                    <hr/>
+                            </div>
+
+                            <div className="col-md-3">
+
+                                <img className="img-fluid" src="./client/src/assets/img/logo/LogoSample_ByTailorBrands.jpg" alt=""></img>
+                            </div>
+
+                            <div className="col- md-9">
+                               <p>
+
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quos tempora quas quibusdam. Maxime laborum a velit id consequatur autem corporis. Officiis vitae corrupti eius recusandae mollitia!
+                                   Eaque, exercitationem adipisci.
+                               </p>
+
+                                <p>
+
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quos tempora quas quibusdam. Maxime laborum a velit id consequatur autem corporis. Officiis vitae corrupti eius recusandae mollitia!
+                                    Eaque, exercitationem adipisci.
+                               </p>
+
+                                <p>
+
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quos tempora quas quibusdam. Maxime laborum a velit id consequatur autem corporis. Officiis vitae corrupti eius recusandae mollitia!
+                                    Eaque, exercitationem adipisci.
+                               </p>
+
+                                <hr />
+                            </div>
+                        </div>
+                    </div>
+
+            </section>
+
+            </div>
+
+         );
+    }
+}
+
+export default AboutPage;
