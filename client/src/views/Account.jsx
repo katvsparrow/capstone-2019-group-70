1cdd89ce891b2a83ed67cb15163c1eedadf6ab0b
dillanea <32236423+dillanea@users.noreply.github.com>
2020-05-10 21:38:40 -0700

    Fix account spacing

--- client/src/views/Account.jsx
+++ client/src/views/Account.jsx
@@ -102,7 +102,7 @@ class Account extends React.Component {
                 </section>  
                 <section className="section">
                     <Container>
-                        <Card className="card-profile shadow mt--300">
+                        <Card className="card-profile shadow mt--450">
                             <div className="px-4">
                                 <Row className="justify-content-center">
                                     <Col className="order-lg-2" lg="3">
