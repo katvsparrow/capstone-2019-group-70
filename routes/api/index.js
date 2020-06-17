cf3f98f9e10715a926c7f0c32d54f5da25922ed6
dillanea <32236423+dillanea@users.noreply.github.com>
2020-06-17 14:30:21 -0700

    Formatting

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 var router = require('express').Router();
 
-// Include all api routes 
+// Include all api routes
 router.use('/users', require('./users'));
 router.use('/documents', require('./documents'));
 
-// Error handler
+// Error handler, passes to main server
 router.use(function(err, req, res, next) {
     return next(err);
 });
